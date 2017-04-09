7e0aabb97d13da50c42ef2f4b54e32de7c3d2713
Higgs_S <maxwelllee54@gmail.com>
2017-04-09 12:17:26 -0400

    Add files via upload

---
+++ input_pre.py
@@ -0,0 +1,13 @@
+import pandas as pd
+path = '/Users/Maxwell/PycharmProjects/Github/Rapid_Assessment_Tools/SharedFiles/Fordham/RIA Bhutan/Docs Reviewed/Bhutan_Input.txt'
+
+with open(path, 'r') as f:
+    text = f.read()
+
+
+df = pd.DataFrame([context.strip().split(' ', 1) for context in text.split(sep='*****')], columns=['sector', 'text'])
+
+df.to_csv('bhutan_input.csv')
+
+
+
